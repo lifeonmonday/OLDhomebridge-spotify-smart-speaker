5ad2a06403393fc24501ebee9ddb4bf4c69e0166
piotr <19289194+lifeonmonday@users.noreply.github.com>
2026-08-19 22:04:08 +0200

    Delete src directory

--- src/settings.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This must match the 'name' in your package.json
- */
-export const PLUGIN_NAME = 'homebridge-spotify-smart-speaker';
-
-/**
- * This must match the platform name used in Homebridge's config.json
- */
-export const PLATFORM_NAME = 'SpotifySmartSpeaker';
